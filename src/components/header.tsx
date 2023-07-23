2bce179456d7c23bd456893780610e3e7fb4e089
Raphael <plassa_r@etna-alternance.net>
2023-07-23 21:26:36 +0200

    ✨ Init dev - V2

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Header() {
+  return (
+    <header className="flex justify-center items-center mt-2  gap-20">
+      <img src="/assets/logos/raphotos.png" alt="logo" className="w-12" />
+      <div>Raphotos</div>
+      <div>Découvrir</div>
+      <div>Rechercher</div>
+      <div>A propos </div>
+    </header>
+  );
+}
