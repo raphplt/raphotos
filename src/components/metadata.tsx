82f8fcedf38a0faaf795c4d6aa74a59f18682bd2
Raphael <plassa_r@etna-alternance.net>
2023-07-23 21:26:36 +0200

    ✨ Init dev - V2

---
+++ src/components/metadata.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Metadata() {
+  return (
+    <Head>
+      <title>Raphotos</title>
+      <meta name="description" content="Raphotos" />
+      <link rel="icon" href="favicon.ico" />
+    </Head>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/header";
+import Metadata from "@/components/metadata";
+
+export default function Index() {
+  return (
+    <div>
+      <Metadata />
+      <Header />
+      <h1>Raphotos</h1>
+    </div>
+  );
+}
