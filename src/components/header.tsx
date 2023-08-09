b8735ad507553dc0acb5612f9980eeed12548ae2
plassa_r <plassa_r@etna-alternance.net>
2023-08-09 23:10:16 +0200

    ✨ Work on frontend

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,29 @@
+import Link from "next/link";
+
 /* eslint-disable @next/next/no-img-element */
 export default function Header() {
   return (
-    <header className="flex justify-center items-center mt-2  gap-20">
-      <img src="/assets/logos/raphotos.png" alt="logo" className="w-12" />
-      <div>Raphotos</div>
-      <div>Découvrir</div>
-      <div>Rechercher</div>
-      <div>A propos </div>
+    <header className="flex justify-center gap-6 sm:gap-0 z-10 bg-white py-4 top-0 sm:justify-around items-center sm:mx-20 w-[100vw] fixed ">
+      <Link href={"/"} className="hover:underline underline-offset-2">
+        Raphotos
+      </Link>
+      <div className="flex gap-2 sm:gap-20">
+        <Link
+          href={"/decouvrir"}
+          className="hover:underline underline-offset-2"
+        >
+          Découvrir
+        </Link>
+        <Link href={"/blog"} className="hover:underline underline-offset-2">
+          Blog
+        </Link>
+        <Link href={"/shop"} className="hover:underline underline-offset-2">
+          Shop
+        </Link>
+        <Link href={"/a-propos"} className="hover:underline underline-offset-2">
+          A propos
+        </Link>
+      </div>
     </header>
   );
 }
