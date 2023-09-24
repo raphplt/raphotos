4c122dd17295ba1590d88780ccd9c8ecb815ccf8
plassa_r <plassa_r@etna-alternance.net>
2023-09-24 10:28:16 +0200

    ✨ Update wording

--- src/pages/a-propos.tsx
+++ src/pages/a-propos.tsx
@@ -1,13 +1,14 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Metadata from "@/components/metadata";
+import Image from "next/image";
 
 export default function APropos() {
   return (
     <div>
       <Metadata title="A propos" />
       <Header />
-      <div className="mt-24 mb-12 sm:h-[100vh]">
+      <div className="mt-24 mb-12">
         <h1 className="mt-28 text-2xl text-center bg-black text-white w-fit mx-auto px-24 py-1">
           A propos
         </h1>
@@ -15,22 +16,29 @@ export default function APropos() {
           Bienvenue sur mon site web dédié à la photographie ! Je m&apos;appelle
           Raphaël, j&apos;ai 19 ans et je suis un passioné de photos depuis
           plusieurs années. Je photographie principalement des paysages, mais je
-          m&apos;essaie aussi aux portraits où à d&apos;autres styles de
+          m&apos;essaie aussi aux portraits oo à d&apos;autres styles de
           photographies.
+          <Image
+            src={"/assets/others/moi.jpg"}
+            alt="Moi"
+            width={1000}
+            height={700}
+            className="w-2/3 mx-auto py-5"
+          />
           <br />A travers mes clichés, j&apos;essaie de mettre en valeur la
           poésie qui se cache dans les paysages et les détails qu&apos;on ne
           regarde pas.
           <br />
           <br />
           Mon matériel actuel:
-          <br />- Sony Alph 6000
+          <br />- Sony Alpha 6000
           <br />- Objectif Sony 16-50mm
           <br />- Objectif Sony 55-210mm
           <br />- Trépied Boulanger Essentiels
           <br />
           <br />
-          Toutes les photos sont téléchargables et utilisables sous la licnce CC
-          by NC (pas d&apos;utilisation commerciale).
+          Toutes les photos sont téléchargables et utilisables sous la licence
+          CC by NC (pas d&apos;utilisation commerciale).
           <br />
           <br />
           Le site en est actuellement à sa version 2, en date du 23/09/2023. Si
