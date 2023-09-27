7ec2e8c0de24758b125681e76d77eccfe4fe7dbe
Raphael <plassa_r@etna-alternance.net>
2023-09-27 08:59:48 +0200

    🐛 Update vidéo logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -106,7 +106,7 @@ export default function Slider({ imagePaths }: any) {
           <Card
             title="Mes dernières vidéos"
             href={"/videos"}
-            img="/assets/pictures/Summer_2023/DSC06232-min.jpg"
+            img="/assets/pictures/Summer_2023/DSC06294-min.jpg"
           />
           <Card
             title="A propos de moi"
