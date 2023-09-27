ea7830453221411b8da5f96b29bc98591535e7d7
Raphael <plassa_r@etna-alternance.net>
2023-09-27 09:41:10 +0200

    ✨ Add cookies

---
+++ src/pages/cookies.tsx
@@ -0,0 +1,225 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Metadata from "@/components/metadata";
+
+export default function Cookies() {
+  return (
+    <div>
+      <Metadata title="Cookies" />
+      <Header />
+      <div className="w-2/3  mt-36 text-center mx-auto mb-12">
+        <h1 className="text-xl mb-4 mt-6">Politique relative aux cookies</h1>
+        <p>Dernière mise à jour : 27 septembre 2023</p>
+        <p>
+          Cette Politique relative aux cookies explique ce que sont les cookies
+          et comment nous les utilisons. Vous devriez lire cette politique afin
+          de comprendre quel type de cookies nous utilisons, les informations
+          que nous collectons à l&apos;aide de ces cookies et comment ces
+          informations sont utilisées.
+        </p>
+        <p>
+          Les cookies ne contiennent généralement aucune information permettant
+          d&apos;identifier personnellement un utilisateur, mais des
+          informations personnelles que nous stockons à votre sujet peuvent être
+          liées aux informations stockées et obtenues à partir des cookies. Pour
+          plus d&apos;informations sur la manière dont nous utilisons, stockons
+          et sécurisons vos données personnelles, consultez notre Politique de
+          confidentialité.
+        </p>
+        <p>
+          Nous ne stockons pas d&apos;informations personnelles sensibles,
+          telles que des adresses postales, des mots de passe de compte, etc.,
+          dans les cookies que nous utilisons.
+        </p>
+        <h1 className="text-xl mb-4 mt-6">Interprétation et Définitions</h1>
+        <h2>Interprétation</h2>
+        <p>
+          Les mots dont la première lettre est en majuscule ont des
+          significations définies dans les conditions suivantes. Les définitions
+          suivantes auront la même signification, qu&apos;elles apparaissent au
+          singulier ou au pluriel.
+        </p>
+        <h2>Définitions</h2>
+        <p>Pour les besoins de cette Politique relative aux cookies :</p>
+        <ul>
+          <li>
+            <strong>Entreprise</strong> (dénommée soit « l&apos;Entreprise, «
+            Nous », «Notre» ou «Nos» dans cette Politique relative aux cookies)
+            fait référence à Raphotos.
+          </li>
+          <li>
+            <strong>Cookies</strong> désigne de petits fichiers placés sur votre
+            ordinateur, votre appareil mobile ou tout autre appareil par un site
+            web, contenant des détails sur votre historique de navigation sur ce
+            site parmi ses nombreuses utilisations.
+          </li>
+          <li>
+            <strong>Site web</strong> fait référence à Raphotos, accessible
+            depuis{" "}
+            <a
+              href="https://www.raphotos.fr"
+              rel="external nofollow noopener"
+              target="_blank"
+            >
+              https://www.raphotos.fr
+            </a>
+          </li>
+          <li>
+            <strong>Vous</strong> désigne l&apos;entité légale au nom de
+            laquelle cet individu accède ou utilise le site web, le cas échéant.
+          </li>
+        </ul>
+        <h1 className="text-xl mb-4 mt-6">L&apos;utilisation des cookies</h1>
+        <h2>Type de cookies que nous utilisons</h2>
+        <p>
+          Les cookies peuvent être des «cookies persistants» ou des «cookies de
+          session». Les cookies persistants demeurent sur votre ordinateur
+          personnel ou votre appareil mobile lorsque vous êtes hors ligne,
+          tandis que les cookies de session sont supprimés dès que vous fermez
+          votre navigateur web.
+        </p>
+        <p>
+          Nous utilisons à la fois des cookies de session et des cookies
+          persistants aux fins énoncées ci-dessous :
+        </p>
+        <ul>
+          <li>
+            <p>
+              <strong>Cookies nécessaires / essentiels</strong>
+            </p>
+            <p>Type : Cookies de session</p>
+            <p>Administrés par : Nous</p>
+            <p>
+              Objectif : Ces cookies sont essentiels pour vous fournir les
+              services disponibles sur le site web et vous permettre
+              d&apos;utiliser certaines de ses fonctionnalités. Ils contribuent
+              à authentifier les utilisateurs et à prévenir toute utilisation
+              frauduleuse des comptes d&apos;utilisateur. Sans ces cookies, les
+              services que vous avez demandés ne peuvent pas être fournis, et
+              nous n&apos;utilisons ces cookies que pour vous fournir ces
+              services.
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Cookies de fonctionnalité</strong>
+            </p>
+            <p>Type : Cookies persistants</p>
+            <p>Administrés par : Nous</p>
+            <p>
+              Objectif : Ces cookies nous permettent de mémoriser les choix que
+              vous faites lorsque vous utilisez le site web, tels que la
+              mémorisation de vos informations de connexion ou de vos
+              préférences linguistiques. Le but de ces cookies est de vous
+              offrir une expérience plus personnelle et d&apos;éviter que vous
+              ayez à réintroduire vos préférences à chaque fois que vous
+              utilisez le site web.
+            </p>
+          </li>
+        </ul>
+        <h2>Vos choix concernant les cookies</h2>
+        <p>
+          Si vous préférez éviter l&apos;utilisation de cookies sur le site web,
+          vous devez d&apos;abord désactiver l&apos;utilisation des cookies dans
+          votre navigateur, puis supprimer les cookies enregistrés dans votre
+          navigateur associés à ce site web. Vous pouvez utiliser cette option
+          pour empêcher l&apos;utilisation de cookies à tout moment.
+        </p>
+        <p>
+          Si vous n&apos;acceptez pas nos cookies, vous pouvez rencontrer des
+          désagréments lors de l&apos;utilisation du site web, et certaines
+          fonctionnalités peuvent ne pas fonctionner correctement.
+        </p>
+        <p>
+          Si vous souhaitez supprimer les cookies ou donner des instructions à
+          votre navigateur web pour supprimer ou refuser les cookies, veuillez
+          consulter les pages d&apos;aide de votre navigateur web.
+        </p>
+        <ul className="flex flex-col gap-10 mt-6">
+          <li>
+            <p>
+              Pour le navigateur web Chrome, veuillez visiter cette page de
+              Google :
+              <a
+                href="https://support.google.com/accounts/answer/32050"
+                rel="external nofollow noopener"
+                target="_blank"
+              >
+                https://support.google.com/accounts/answer/32050
+              </a>
+            </p>
+          </li>
+          <li>
+            <p>
+              Pour le navigateur web Internet Explorer, veuillez visiter cette
+              page de Microsoft :
+              <a
+                href="http://support.microsoft.com/kb/278835"
+                rel="external nofollow noopener"
+                target="_blank"
+              >
+                http://support.microsoft.com/kb/278835
+              </a>
+            </p>
+          </li>
+          <li>
+            <p>
+              Pour le navigateur web Firefox, veuillez visiter cette page de
+              Mozilla :
+              <a
+                href="https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored"
+                rel="external nofollow noopener"
+                target="_blank"
+              >
+                https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored
+              </a>
+            </p>
+          </li>
+          <li>
+            <p>
+              Pour le navigateur web Safari, veuillez visiter cette page
+              d&apos;Apple :
+              <a
+                href="https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac"
+                rel="external nofollow noopener"
+                target="_blank"
+              >
+                https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac
+              </a>
+            </p>
+          </li>
+        </ul>
+        <p>
+          Pour tout autre navigateur web, veuillez consulter les pages
+          officielles de votre navigateur web.
+        </p>
+        <h2>Plus d&apos;informations sur les cookies</h2>
+        <p>
+          Vous pouvez en apprendre davantage sur les cookies ici :
+          <a href="https://www.termsfeed.com/blog/cookies/" target="_blank">
+            Tout sur les cookies par TermsFeed
+          </a>
+          .
+        </p>
+        <h2>Contactez-nous</h2>
+        <p>
+          Si vous avez des questions concernant cette Politique relative aux
+          cookies, vous pouvez nous contacter :
+        </p>
+        <ul>
+          <li>
+            En visitant cette page sur notre site web :
+            <a
+              href="https://www.raphotos.fr/cookies"
+              rel="external nofollow noopener"
+              target="_blank"
+            >
+              https://www.raphotos.fr/cookies
+            </a>
+          </li>
+        </ul>
+      </div>
+      <Footer />
+    </div>
+  );
+}
